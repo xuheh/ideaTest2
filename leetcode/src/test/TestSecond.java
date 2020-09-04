32fccea04b2db5fdda1c35f824d68d03ff984c9a
xuheheaa <1227991378@qq.com>
2020-09-04 11:50:10 +0800

    addSecondClass

---
+++ leetcode/src/test/TestSecond.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @Author: xhh
+ * @Date: 2020/9/4 11:49
+ * @Version 1.0
+ */
+public class TestSecond {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
